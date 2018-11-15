0eb0d0b905574ee47898e8c0d2ec22e5ee5b7df2
Idris Adetunmbi <idrisadetunmbi@gmail.com>
2018-11-15 23:17:36 +0100

    implement CRUD endpoints for items

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,11 @@ import bodyParser from 'body-parser';
 import express from 'express';
 import http from 'http';
 import logger from 'morgan';
+import mongoose from 'mongoose';
 
 import config from './config';
 import webpackMiddleware from '../../webpack-configs/middleware';
+import router from './routes';
 
 require('dotenv').config();
 
@@ -13,11 +15,13 @@ app.use(logger('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use('/api', router);
+
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   webpackMiddleware(app);
 }
 
 const port = Number(config.PORT || 3000);
 http.createServer(app).listen(port, () => {
-  console.log(`Server started successfully on port ${port}`);
+  mongoose.connect(config.DATABASE_URL);
 });
---
+++ src/server/models/GroceryItem.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 25,
+  },
+  purchased: { type: Boolean, default: false },
+});
+
+export default mongoose.model('GroceryItem', schema, 'grocery_items');
